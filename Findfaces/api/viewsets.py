9c0bc61d02e724f25716920ef5e02da05c95e763
davidpereiras <davidpereira8370@gmail.com>
2021-11-30 10:55:32 -0300

    Revert "first commit"
    This reverts commit 8f7f2e7895c53626f5c129698d7b0170c8512750.

--- Findfaces/api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from Findfaces.api import serializers
-from Findfaces import models 
-
-class FindfacesViewset(viewsets.ModelViewSet):
- serializer_class = serializers.FindFaceSerializer
- queryset = models.img.objects.all()
